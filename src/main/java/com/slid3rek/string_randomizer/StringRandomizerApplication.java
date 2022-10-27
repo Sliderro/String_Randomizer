0618ad4b1e88ca3f64f087dc8cf1abf312c234cd
sliderro <arek.debski2@gmail.com>
2022-10-27 23:55:27 +0200

    First Commit - Creating the base of an application using Spring Initializr

---
+++ src/main/java/com/slid3rek/string_randomizer/StringRandomizerApplication.java
@@ -0,0 +1,13 @@
+package com.slid3rek.string_randomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringRandomizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringRandomizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slid3rek/string_randomizer/StringRandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slid3rek.string_randomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringRandomizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
