7bf25c49d16ab49eda211de59b8ff460072341f5
sliderro <arek.debski2@gmail.com>
2022-10-29 02:26:30 +0200

    Created docker files, added test mapping, added jobs table.

---
+++ string_randomizer/src/main/java/com/slid3rek/string_randomizer/jpa/Job.java
@@ -0,0 +1,81 @@
+package com.slid3rek.string_randomizer.jpa;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "jobs")
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    private Integer min;
+    private Integer max;
+    private String charset;
+    private Boolean isRunning;
+
+    private String url;
+
+    public Job(Integer min, Integer max, String charset, Boolean isRunning, String url){
+        this.min        = min;
+        this.max        = max;
+        this.charset    = charset;
+        this.isRunning  = isRunning;
+        this.url = url;
+    }
+
+    public Job() {
+
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMin() {
+        return min;
+    }
+
+    public void setMin(Integer min) {
+        this.min = min;
+    }
+
+    public Integer getMax() {
+        return max;
+    }
+
+    public void setMax(Integer max) {
+        this.max = max;
+    }
+
+    public String getCharset() {
+        return charset;
+    }
+
+    public void setCharset(String charset) {
+        this.charset = charset;
+    }
+
+    public Boolean getRunning() {
+        return isRunning;
+    }
+
+    public void setRunning(Boolean running) {
+        isRunning = running;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
